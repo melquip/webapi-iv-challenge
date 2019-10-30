a49a7bd01bf8f784debdd60943f9d6cea6ec60d7
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-30 12:42:43 +0000

    Plugged in helmet, cors, express and logger middleware

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-const express = 'express';
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const server = express();
+
+server.use(helmet());
+server.use(logger);
+server.use(express.json());
+server.use(cors());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
-
 function logger(req, res, next) {
-
+  console.log(req.method, req.url, Date.now())
 };
 
 module.exports = server;
