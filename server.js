fdd8fbd7f0b99bc81d49100fcdf64fd3f0d07a49
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-31 13:22:38 +0000

    Added missing required dependency

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const path = require('path');
 
 const userRouter = require('./users/userRouter');
 const postRouter = require('./posts/postRouter');
