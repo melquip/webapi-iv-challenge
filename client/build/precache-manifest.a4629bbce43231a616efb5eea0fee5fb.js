3a1ef104c2cedf665c025aa7110426148e66fff9
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-31 13:28:59 +0000

    Built react app

---
+++ client/build/precache-manifest.a4629bbce43231a616efb5eea0fee5fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9df224eebbfcb3bb8b56400d9ff0f63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b23fc8bcbabea3bba6c2",
+    "url": "/static/css/main.fb4c04cd.chunk.css"
+  },
+  {
+    "revision": "e13349981c9c13ea8a4e",
+    "url": "/static/js/2.7b5261f1.chunk.js"
+  },
+  {
+    "revision": "b23fc8bcbabea3bba6c2",
+    "url": "/static/js/main.d2bc96f9.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
